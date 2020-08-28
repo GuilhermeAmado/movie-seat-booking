15fb27e6c705fe2d5b2f69dabb8521cafe2c1711
Guilherme <guiii@live.com>
2020-08-28 00:52:46 -0300

    Add functionality to save to localstorage

--- script.js
+++ script.js
@@ -5,14 +5,29 @@ const totalCost = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = parseInt(movieSelect.value);
 
+function saveMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const selectedSeatsCount = selectedSeats.length;
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    console.log(seatsIndex);
+
     seatCount.innerText = selectedSeatsCount;
     totalCost.innerText = selectedSeatsCount * ticketPrice;
 }
 
-movieSelect.addEventListener('change', e => ticketPrice = parseInt(e.target.value));
+movieSelect.addEventListener('change', e => {
+    ticketPrice = parseInt(e.target.value);
+    saveMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+});
 
 seatsContainer.addEventListener('click', e => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
